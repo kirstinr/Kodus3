33e61a8fa4171b5f960ea03c940a23dd49e2476e
Kristin Raudsepp <kristin.raudsepp@tigu.hk.tlu.ee>
2016-10-29 10:11:14 +0300

    siin ka korda

--- Proov.java
+++ Proov.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class Proov{
 	public static void main(String[] arg){
 		Kindad k1=new Kindad(2, 3, 1.5);
-		System.out.println(k1.MyygiHind());
 		List<LisaHind> kasitoo=new ArrayList<LisaHind>();
 		kasitoo.add(k1);
 		kasitoo.add(new LapiTekk(3, 4, 5, 1.13, 1.5));
